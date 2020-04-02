9c98480624aba954f9b30926ff4783f017745a0b
Gordon_Fow <49438374+Zaraley@users.noreply.github.com>
2020-04-02 14:35:01 +0300

    Add files via upload

---
+++ engine/config.php
@@ -0,0 +1,23 @@
+<?php
+$cfg = [
+	'site_name' => '149.154.66.135',
+	'site_title' => 'Лучший дроп CS:GO',
+	'db_host' => 'localhost',
+    'db_user' => 'root',
+    'db_pass' => '558137an',
+    'db_name' => 'root',
+
+
+   'steamapi' => '0C06F67E614B69BAAD6243AB4A70DE36',
+   'bot_profile' => 'http://steamcommunity.com/id/Anri_ger/',  // Пример: вот ссылка https://steamcommunity.com/id/playdropbot/ должно получиться так 'bot_profile' => 'playdropbot'
+
+    //'merchant_id' => '123',
+    'merchant_id' => 'Ид Магазина',
+    'merchant_secret_1' => 'секрет 1',
+    'merchant_secret_2' => 'секрет 2',
+
+	'merchant_secret_1_test' => '',
+    'merchant_secret_2_test' => ''
+];
+
+?>
